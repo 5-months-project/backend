8dd1fd75c5d056561cff97f58123d2bd93e94061
yunzae <dsa05037@naver.com>
2023-02-06 05:19:18 +0900

    DTO추가

---
+++ gongda/src/main/java/projectbusan/gongda/dto/GroupEnterDTO.java
@@ -0,0 +1,13 @@
+package projectbusan.gongda.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
+public class GroupEnterDTO {
+    private String groupCode;
+    private String password;
+
+}
