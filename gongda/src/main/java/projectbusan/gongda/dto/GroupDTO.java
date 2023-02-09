e03be037bbcb4047b4bdb1bd681480950eabffb1
yunzae <dsa05037@naver.com>
2023-02-10 08:22:38 +0900

    1차적인 기능구현완료, 테스트및 접근권한확인기능 추가필요

--- gongda/src/main/java/projectbusan/gongda/dto/GroupDTO.java
+++ gongda/src/main/java/projectbusan/gongda/dto/GroupDTO.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class GroupDTO {
-    private String name;
-    private String groupCode;
+    private String groupname;
+    private String groupcode;
 
 
 }
