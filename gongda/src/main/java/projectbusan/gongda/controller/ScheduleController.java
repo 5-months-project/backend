ef2eff3035aafcc8a1ce5b9a428bdf19f1f098bf
yunzae <dsa05037@naver.com>
2023-02-11 20:13:58 +0900

    테스트완료

--- gongda/src/main/java/projectbusan/gongda/controller/ScheduleController.java
+++ gongda/src/main/java/projectbusan/gongda/controller/ScheduleController.java
@@ -1,9 +1,14 @@
 package projectbusan.gongda.controller;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import projectbusan.gongda.dto.ResultDTO;
 import projectbusan.gongda.dto.ScheduleCreateDTO;
@@ -15,6 +20,7 @@ import projectbusan.gongda.repository.UserRepository;
 import projectbusan.gongda.service.ScheduleService;
 
 import javax.validation.Valid;
+import java.security.Key;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,6 +30,7 @@ import java.util.Optional;
 public class ScheduleController {
     private final ScheduleService scheduleService;
     private final UserRepository userRepository;
+    private final String secret ="26630e34ffad38ac9c311b6521df32ce27b53c6c3a87403b79ed7bb09f836d9cf746b0d75ae42abd7845927f81b8343ee2fa292946cf67cfac95444a19c29127";
 
     @Autowired
     public ScheduleController(ScheduleService scheduleService, UserRepository userRepository) {
@@ -36,7 +43,8 @@ public class ScheduleController {
     /*개인스케쥴-생성  생성시 날짜벌로 객체가 생성됨, 예를 들어 2/1~2/3인 스케쥴을 생성시 2/1,2/2,2/3 3개의 객체가 생성되고 이 셋은 동일한 code을 가짐 */
     @PostMapping("/schedule")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<ScheduleDTO> create(@Valid @RequestBody ScheduleCreateDTO scheduleCreateDTO, @RequestParam String useremail){
+    public ResponseEntity<ScheduleDTO> create(@Valid @RequestBody ScheduleCreateDTO scheduleCreateDTO, HttpServletRequest request){
+        String useremail = getUseremailByToken(request);
         User user =userRepository.findOneWithAuthoritiesByUsername(useremail).get();
         return ResponseEntity.ok(scheduleService.create(scheduleCreateDTO,user,ScheduleService.codeCreate()));
     }
@@ -44,7 +52,8 @@ public class ScheduleController {
     /*개인스케쥴-날짜별조회*/
     @GetMapping("/schedule-date/{date}")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<ResultDTO> read(@PathVariable Long date,@RequestParam String useremail){
+    public ResponseEntity<ResultDTO> read(@PathVariable Long date,HttpServletRequest request){
+        String useremail = getUseremailByToken(request);
         Optional<User> opuser =userRepository.findOneWithAuthoritiesByUsername(useremail);
         if (opuser.isEmpty()) throw new NotFoundMemberException("해당 유저를 찾을 수 없습니다.");
         User user =opuser.get();
@@ -55,7 +64,8 @@ public class ScheduleController {
     /*개인스케쥴-수정 (name,content,category 수정만됨, 시간수정시에는 삭제후 재등록해야함)*/
     @PutMapping("/schedule")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<ScheduleDTO> update(@Valid @RequestBody ScheduleModifyDTO scheduleModifyDTO,@RequestParam String useremail){
+    public ResponseEntity<ScheduleDTO> update(@Valid @RequestBody ScheduleModifyDTO scheduleModifyDTO,HttpServletRequest request){
+        String useremail = getUseremailByToken(request);
         Optional<User> opuser =userRepository.findOneWithAuthoritiesByUsername(useremail);
         if (opuser.isEmpty()) throw new NotFoundMemberException("해당 유저를 찾을 수 없습니다.");
         User user =opuser.get();
@@ -65,7 +75,8 @@ public class ScheduleController {
     /*개인스케쥴-삭제*/
     @DeleteMapping("/schedule/{schedulecode}")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<ScheduleDTO> delete(@PathVariable String schedulecode,@RequestParam String useremail){
+    public ResponseEntity<ScheduleDTO> delete(@PathVariable String schedulecode,HttpServletRequest request){
+        String useremail = getUseremailByToken(request);
         Optional<User> opuser =userRepository.findOneWithAuthoritiesByUsername(useremail);
         if (opuser.isEmpty()) throw new NotFoundMemberException("해당 유저를 찾을 수 없습니다.");
         User user =opuser.get();
@@ -76,9 +87,10 @@ public class ScheduleController {
     /*개인의 모든 스케쥴 가져오기, DTO만들어서 하나처럼*/
     @GetMapping("/schedules")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<ResultDTO> readAll(@RequestParam String useremail){
+    public ResponseEntity<ResultDTO> readAll(HttpServletRequest request){
+        String useremail = getUseremailByToken(request);
         Optional<User> opuser =userRepository.findOneWithAuthoritiesByUsername(useremail);
-        if (opuser.isEmpty()) throw new NotFoundMemberException("해당 유저를 찾을 수 없습니다.");
+        if (opuser.isEmpty()) throw new NotFoundMemberException("해당 유저를 찾을 수 없습니다. ");
         User user =opuser.get();
         ResultDTO<List> resultDTO = new ResultDTO<>(scheduleService.readAll(user));
         return ResponseEntity.ok(resultDTO);
@@ -89,7 +101,8 @@ public class ScheduleController {
     /*그룹스케쥴-생성*/
     @PostMapping("/group-schedule/{group_code}")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<ScheduleDTO> group_create(@Valid @RequestBody ScheduleCreateDTO scheduleCreateDTO, @PathVariable String group_code,@RequestParam String useremail){
+    public ResponseEntity<ScheduleDTO> group_create(@Valid @RequestBody ScheduleCreateDTO scheduleCreateDTO, @PathVariable String group_code,HttpServletRequest request){
+        String useremail = getUseremailByToken(request);
         Optional<User> opuser =userRepository.findOneWithAuthoritiesByUsername(useremail);
         if (opuser.isEmpty()) throw new NotFoundMemberException("해당 유저를 찾을 수 없습니다.");
         User user =opuser.get();
@@ -99,7 +112,8 @@ public class ScheduleController {
     /*그룹스케쥴-날짜별 조회*/
     @GetMapping("/group-schedule-date/{group_code}/{date}")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<ResultDTO> group_read(@PathVariable String group_code,@PathVariable Long date,@RequestParam String useremail){
+    public ResponseEntity<ResultDTO> group_read(@PathVariable String group_code,@PathVariable Long date,HttpServletRequest request){
+        String useremail = getUseremailByToken(request);
         Optional<User> opuser =userRepository.findOneWithAuthoritiesByUsername(useremail);
         if (opuser.isEmpty()) throw new NotFoundMemberException("해당 유저를 찾을 수 없습니다.");
         User user =opuser.get();
@@ -110,7 +124,8 @@ public class ScheduleController {
     /*그룹스케쥴-수정*/
     @PutMapping("/group-schedule")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<ScheduleDTO> group_update(@Valid @RequestBody ScheduleModifyDTO scheduleModifyDTO,@RequestParam String useremail){
+    public ResponseEntity<ScheduleDTO> group_update(@Valid @RequestBody ScheduleModifyDTO scheduleModifyDTO,HttpServletRequest request){
+        String useremail = getUseremailByToken(request);
         Optional<User> opuser =userRepository.findOneWithAuthoritiesByUsername(useremail);
         if (opuser.isEmpty()) throw new NotFoundMemberException("해당 유저를 찾을 수 없습니다.");
         User user =opuser.get();
@@ -120,7 +135,8 @@ public class ScheduleController {
     //todo 얘가 그룹에 속해있는지 체크
     @DeleteMapping("/group-schedule/{schedule_code}")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<ScheduleDTO> group_delete(@PathVariable String schedule_code, @RequestParam String useremail){
+    public ResponseEntity<ScheduleDTO> group_delete(@PathVariable String schedule_code, HttpServletRequest request){
+        String useremail = getUseremailByToken(request);
         Optional<User> opuser =userRepository.findOneWithAuthoritiesByUsername(useremail);
         if (opuser.isEmpty()) throw new NotFoundMemberException("해당 유저를 찾을 수 없습니다.");
         User user =opuser.get();
@@ -132,7 +148,8 @@ public class ScheduleController {
     //todo 얘가 그룹에 속해있는지 체크
     @GetMapping("/group-schedules/{group_code}")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<ResultDTO> group_readAll(@PathVariable String group_code,@RequestParam String useremail){
+    public ResponseEntity<ResultDTO> group_readAll(@PathVariable String group_code,HttpServletRequest request){
+        String useremail = getUseremailByToken(request);
         Optional<User> opuser =userRepository.findOneWithAuthoritiesByUsername(useremail);
         if (opuser.isEmpty()) throw new NotFoundMemberException("해당 유저를 찾을 수 없습니다.");
         User user =opuser.get();
@@ -144,7 +161,8 @@ public class ScheduleController {
     /*코드로 일정 가져오기(개인,그룹 상관X) , DTO 만들어서 하나의 일정처럼 보이게 내보내기*/
     @GetMapping("/schedule/{schedule_code}")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<ScheduleDTO> group_read(@PathVariable String schedule_code,@RequestParam String useremail){
+    public ResponseEntity<ScheduleDTO> group_read(@PathVariable String schedule_code,HttpServletRequest request){
+        String useremail = getUseremailByToken(request);
         Optional<User> opuser =userRepository.findOneWithAuthoritiesByUsername(useremail);
         if (opuser.isEmpty()) throw new NotFoundMemberException("해당 유저를 찾을 수 없습니다.");
         User user =opuser.get();
@@ -154,7 +172,8 @@ public class ScheduleController {
     /*개인,그룹 스케쥴-월별통합조회*/
     @GetMapping("/schedule-month/{month}")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<ResultDTO> readByMonth(@PathVariable Long month,@RequestParam String useremail){
+    public ResponseEntity<ResultDTO> readByMonth(@PathVariable Long month,HttpServletRequest request){
+        String useremail = getUseremailByToken(request);
         Optional<User> opuser =userRepository.findOneWithAuthoritiesByUsername(useremail);
         if (opuser.isEmpty()) throw new NotFoundMemberException("해당 유저를 찾을 수 없습니다.");
         User user =opuser.get();
@@ -165,7 +184,8 @@ public class ScheduleController {
     /*개인,그룹스케쥴-다가오는 일정통합조회*/
     @GetMapping("/schedule-comming/{todaydate}")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<ResultDTO> readCommings(@PathVariable Long todaydate,@RequestParam String useremail){
+    public ResponseEntity<ResultDTO> readCommings(@PathVariable Long todaydate,HttpServletRequest request){
+        String useremail = getUseremailByToken(request);
         Optional<User> opuser =userRepository.findOneWithAuthoritiesByUsername(useremail);
         if (opuser.isEmpty()) throw new NotFoundMemberException("해당 유저를 찾을 수 없습니다.");
         User user =opuser.get();
@@ -176,7 +196,8 @@ public class ScheduleController {
     /*개인,그룹스케쥴-날짜별 일정통합조회*/
     @GetMapping("/all-schedule-date/{date}")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<ResultDTO> allReadByDate(@PathVariable Long date,@RequestParam String useremail){
+    public ResponseEntity<ResultDTO> allReadByDate(@PathVariable Long date,HttpServletRequest request){
+        String useremail = getUseremailByToken(request);
         Optional<User> opuser =userRepository.findOneWithAuthoritiesByUsername(useremail);
         if (opuser.isEmpty()) throw new NotFoundMemberException("해당 유저를 찾을 수 없습니다.");
         User user =opuser.get();
@@ -184,4 +205,21 @@ public class ScheduleController {
         return ResponseEntity.ok(resultDTO);
     }
 
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+
+        return null;
+    }
+
+    private String getUseremailByToken(HttpServletRequest request){
+        byte[] keyBytes= Decoders.BASE64.decode(secret);
+        Key key = Keys.hmacShaKeyFor(keyBytes);
+        Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(resolveToken(request)).getBody();
+        String useremail= claims.getSubject();
+        return useremail;
+    }
 }
